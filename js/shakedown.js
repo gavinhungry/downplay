2f4d4f041def4d7d305a5b6603870ed54fa8268b
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-10 13:49:20 -0800

    Add additional HTML5 attributes to attributeBindings for {{textarea}}

--- js/shakedown.js
+++ js/shakedown.js
@@ -5,9 +5,15 @@
 (function() {
   'use strict';
 
+  // Add Handlebars helper for formatting GFM
   marked.setOptions({ gfm: true });
   Ember.Handlebars.helper('format-markdown', function(md) {
     return new Ember.Handlebars.SafeString(marked(md));
+  });
+
+  // Support additional HTML5 attributes on {{textarea}}
+  Ember.TextArea.reopen({
+    attributeBindings: ['autofocus', 'spellcheck']
   });
 
   var App = Ember.Application.create();
