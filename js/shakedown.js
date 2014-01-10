d5c55efcc039a52f9df76cff98fa33540654c1b4
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-10 10:58:35 -0800

    Add App.IndexRoute

--- js/shakedown.js
+++ js/shakedown.js
@@ -4,4 +4,11 @@
 
 (function() {
   App = Ember.Application.create();
+
+  var model = { markdown: '' };
+
+  App.IndexRoute = Ember.Route.extend({
+    model: function() { return model; }
+  });
+
 })();
