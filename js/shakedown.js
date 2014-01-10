65550acbb40481f98ec70effbf0d1618e3672856
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-10 13:34:50 -0800

    Move GFM to own file, use partials for input/output

--- js/shakedown.js
+++ js/shakedown.js
@@ -3,20 +3,16 @@
  */
 
 (function() {
+  'use strict';
 
   marked.setOptions({ gfm: true });
-
   Ember.Handlebars.helper('format-markdown', function(md) {
     return new Ember.Handlebars.SafeString(marked(md));
   });
 
-
-
-
-  App = Ember.Application.create();
+  var App = Ember.Application.create();
 
   var model = { markdown: '' };
-
   App.IndexRoute = Ember.Route.extend({
     model: function() { return model; }
   });
