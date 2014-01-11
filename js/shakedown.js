412d66cfc3fc77115c179e5151f83c3b6770e1ae
Gavin Lloyd <gavinhungry@gmail.com>
2014-01-11 02:53:56 -0800

    Cleanup

--- js/shakedown.js
+++ js/shakedown.js
@@ -1,20 +1,19 @@
 (function($) {
   'use strict';
 
+  var init_nano = function() {
+    setTimeout(function() {
+      $('#output').nanoScroller({
+        alwaysVisible: true
+      }).each(function() {
+        // we gave the slider a 2px margin, so we must reduce its height
+        var slider = this.nanoscroller.sliderHeight - 4;
+        $(this).find('> .pane > .slider').height(slider);
+      });
+    }, 0)
+  };
+
   $(function() {
-
-    var init_nano = function() {
-      setTimeout(function() {
-        $('#output').nanoScroller({
-          alwaysVisible: true
-        }).each(function() {
-          // we gave the slider a 2px margin, so we must reduce its height
-          var slider = this.nanoscroller.sliderHeight - 4;
-          $(this).find('> .pane > .slider').height(slider);
-        });
-      }, 0)
-    };
-
     CodeMirror.fromTextArea($('#markdown')[0], {
       lineWrapping: true
     }).on('change', function(cm) {
